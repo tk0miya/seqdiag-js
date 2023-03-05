d99dc41ca11e34147650b7b20dcf284ca2b21dc9
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-03-06 02:24:47 +0900

    Implement a simple edge builder

--- src/builder.ts
+++ src/builder.ts
@@ -2,9 +2,11 @@ import * as parser from "./parser";
 import { ASTKinds } from "./parser";
 
 export class Diagram {
+	edges: Edge[];
 	nodes: Node[];
 
 	constructor() {
+		this.edges = [];
 		this.nodes = [];
 	}
 }
@@ -16,6 +18,18 @@ export class Node {
 	constructor(node_id: string) {
 		this.id = node_id;
 		this.label = node_id;
+	}
+}
+
+export class Edge {
+	from: Node;
+	op: string;
+	to: Node;
+
+	constructor(from: Node, op: string, to: Node) {
+		this.from = from;
+		this.op = op;
+		this.to = to;
 	}
 }
 
@@ -34,12 +48,53 @@ class DiagramBuilder {
 				case ASTKinds.node_stmt:
 					this.build_node(stmt);
 					break;
+				case ASTKinds.edge_stmt:
+					this.build_edge(stmt);
+					break;
 			}
 		});
 	}
 
 	private build_node(stmt: parser.node_stmt): void {
 		this.diagram.nodes.push(new Node(stmt.name));
+	}
+
+	private find_or_build_node(name: string): Node {
+		const node = this.diagram.nodes.find((node) => node.id === name);
+		if (node) {
+			return node;
+		} else {
+			const new_node = new Node(name);
+			this.diagram.nodes.push(new_node);
+			return new_node;
+		}
+	}
+
+	private build_edge(stmt: parser.edge_stmt) {
+		const from = this.find_or_build_node(stmt.from);
+		this.build_edge_sub(stmt, from, 0);
+	}
+
+	private build_edge_sub(stmt: parser.edge_stmt, from: Node, index: number) {
+		const { op, target } = stmt.to[index];
+		const to = this.find_or_build_node(target);
+
+		let edge;
+		if (op === "=>") {
+			edge = new Edge(from, "->", to);
+		} else {
+			edge = new Edge(from, op, to);
+		}
+		this.diagram.edges.push(edge);
+
+		if (stmt.to.length > index + 1) {
+			this.build_edge_sub(stmt, to, index + 1);
+		}
+
+		if (op === "=>") {
+			edge = new Edge(from, "<-", to);
+			this.diagram.edges.push(edge);
+		}
 	}
 }
 
--- tests/builder.test.ts
+++ tests/builder.test.ts
@@ -8,6 +8,7 @@ describe("buildDiagram()", () => {
 			const ast = parse("seqdiag {}");
 			const diagram = buildDiagram(ast);
 			assert(diagram !== undefined);
+			expect(diagram.edges.length).toBe(0);
 			expect(diagram.nodes.length).toBe(0);
 		});
 
@@ -15,9 +16,30 @@ describe("buildDiagram()", () => {
 			const ast = parse("seqdiag { A; B; }");
 			const diagram = buildDiagram(ast);
 			assert(diagram !== undefined);
+			expect(diagram.edges.length).toBe(0);
 			expect(diagram.nodes.length).toBe(2);
 			expect(diagram.nodes[0]).toMatchObject({ id: "A", label: "A" });
 			expect(diagram.nodes[1]).toMatchObject({ id: "B", label: "B" });
+		});
+
+		it("can build a diagram having edge definitions", () => {
+			const ast = parse("seqdiag { A -> B -> C; D => E => F }");
+			const diagram = buildDiagram(ast);
+			assert(diagram !== undefined);
+			expect(diagram.edges.length).toBe(6);
+			expect(diagram.edges[0]).toMatchObject({ from: { id: "A" }, op: "->", to: { id: "B" } });
+			expect(diagram.edges[1]).toMatchObject({ from: { id: "B" }, op: "->", to: { id: "C" } });
+			expect(diagram.edges[2]).toMatchObject({ from: { id: "D" }, op: "->", to: { id: "E" } });
+			expect(diagram.edges[3]).toMatchObject({ from: { id: "E" }, op: "->", to: { id: "F" } });
+			expect(diagram.edges[4]).toMatchObject({ from: { id: "E" }, op: "<-", to: { id: "F" } });
+			expect(diagram.edges[5]).toMatchObject({ from: { id: "D" }, op: "<-", to: { id: "E" } });
+			expect(diagram.nodes.length).toBe(6);
+			expect(diagram.nodes[0]).toMatchObject({ id: "A", label: "A" });
+			expect(diagram.nodes[1]).toMatchObject({ id: "B", label: "B" });
+			expect(diagram.nodes[2]).toMatchObject({ id: "C", label: "C" });
+			expect(diagram.nodes[3]).toMatchObject({ id: "D", label: "D" });
+			expect(diagram.nodes[4]).toMatchObject({ id: "E", label: "E" });
+			expect(diagram.nodes[5]).toMatchObject({ id: "F", label: "F" });
 		});
 	});
 });

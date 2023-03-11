c1ce50feb654cf5de399adfe320ab88f813ada42
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-03-11 18:32:05 +0900

    Implement main()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export { parse } from "./parser";
+import { parse } from "./parser";
+import { buildDiagram } from "./builder";
+import { DiagramRenderer } from "./renderer";
 
-export const main = (): void => {};
+export function main(element: Element): void {
+	const source = element.textContent || "";
+	const ast = parse(source);
+	const diagram = buildDiagram(ast);
+
+	if (diagram) {
+		element.textContent = "";
+		const renderer = new DiagramRenderer(diagram, element);
+		renderer.render();
+	}
+}
+
+window.addEventListener("load", () => {
+	document.querySelectorAll(".seqdiag").forEach((elem) => {
+		main(elem);
+	});
+});

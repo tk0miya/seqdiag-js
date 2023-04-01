ec4cf8080e74c03eff63c9433eb1e9454268de38
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-04-02 02:42:07 +0900

    metrics: Add Box.extend() method

--- src/metrics.ts
+++ src/metrics.ts
@@ -52,6 +52,15 @@ export class Box extends Size {
 	center(): Point {
 		return new Point(this.coordinate.x + this.width / 2, this.coordinate.y + this.height / 2);
 	}
+
+	extend({ top, left, right, bottom }: { top?: number; left?: number; right?: number; bottom?: number }): Box {
+		return new Box(
+			this.coordinate.x - (left || 0),
+			this.coordinate.y - (top || 0),
+			this.width + (left || 0) + (right || 0),
+			this.height + (top || 0) + (bottom || 0),
+		);
+	}
 }
 
 export class Metrics {
--- tests/metrics.test.ts
+++ tests/metrics.test.ts
@@ -5,7 +5,7 @@
 import assert from "assert";
 import { parse } from "../src/parser";
 import { buildDiagram } from "../src/builder";
-import { Metrics, Size } from "../src/metrics";
+import { Box, Metrics, Size } from "../src/metrics";
 import { DiagramRenderer } from "../src/renderer";
 
 describe("Size", () => {
@@ -33,6 +33,40 @@ describe("Size", () => {
 			expect(box.coordinate.y).toBe(y);
 			expect(box.width).toBe(width);
 			expect(box.height).toBe(height);
+		});
+	});
+});
+
+describe("Box", () => {
+	describe("new Box()", () => {
+		it("has coordinate, width and height as properties", () => {
+			const width = 123;
+			const height = 456;
+			const x = 78;
+			const y = 90;
+			const box = new Box(x, y, width, height);
+
+			expect(box.coordinate.x).toBe(x);
+			expect(box.coordinate.y).toBe(y);
+			expect(box.width).toBe(width);
+			expect(box.height).toBe(height);
+		});
+	});
+
+	describe("Box.extend()", () => {
+		it("returns an extended Box", () => {
+			const width = 123;
+			const height = 456;
+			const x = 78;
+			const y = 90;
+			const box = new Box(x, y, width, height);
+
+			const [top, left, right, bottom] = [1, 2, 3, 4];
+			const extended = box.extend({ top, left, right, bottom });
+			expect(extended.coordinate.x).toBe(x - left);
+			expect(extended.coordinate.y).toBe(y - top);
+			expect(extended.width).toBe(width + left + right);
+			expect(extended.height).toBe(height + top + bottom);
 		});
 	});
 });

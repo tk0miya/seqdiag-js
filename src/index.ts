e375f637265c00bd75f37dea4de4c06f62f1a421
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-05-05 23:10:12 +0900

    example: Upgrade example/index.html to the interactive shell

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { parse } from "./parser";
 import { buildDiagram } from "./builder";
 import { DiagramRenderer } from "./renderer";
 
-export function main(element: Element): void {
-	const source = element.textContent || "";
+export function build(source: string, destination: Element): void {
 	const ast = parse(source);
 	const diagram = buildDiagram(ast);
 
 	if (diagram) {
-		element.textContent = "";
-		const renderer = new DiagramRenderer(diagram, element);
+		destination.innerHTML = "";
+		const renderer = new DiagramRenderer(diagram, destination);
 		renderer.render();
 	}
 }
 
 window.addEventListener("load", () => {
 	document.querySelectorAll(".seqdiag").forEach((elem) => {
-		main(elem);
+		const source = elem.textContent || "";
+		build(source, elem);
 	});
 });
